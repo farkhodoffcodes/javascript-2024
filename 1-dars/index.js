848183ea4dc35ae4ead9364afd7d6d137c2f4e6b
farkhodoff19 <farkhodoffcodes@gmail.com>
2023-12-02 04:13:35 -0800

    1-dars

---
+++ 1-dars/index.js
@@ -0,0 +1,96 @@
+// console.log("Hello");
+
+// alert("Js is working")
+// prompt("Who are you ? , man")
+// confirm("18+")
+
+// var text = "lorem";
+// let text2 = "lorem";
+// const pi = 3.14;
+// pi = 3.15;
+
+// singleThread
+
+// console.log(pi);
+
+// let s = 2;
+// let b = 3;
+// console.log(s,b);
+
+
+// console.log(s);
+
+// var s = "Salom";
+// var b = "Salom";
+// console.log(b);
+
+
+// PRIMITIVE DATA TYPES
+// let num = 11;
+// console.log(typeof num);
+
+
+// let str = "ss";
+// console.log(typeof str);
+
+// let bigIn = BigInt(2);
+// console.log(typeof bigIn);
+
+// let und;
+// console.log(und);
+
+// let nll = null;
+// console.log(typeof nll);
+
+
+// let sym = Symbol("id");
+// console.log(typeof sym);
+
+// let bool = true;
+
+// console.log(typeof bool);
+
+// REFERENCE DATA TYPES
+
+let obj = {
+    name:"Joe",
+    age:99,
+    isMarried: true,
+}
+
+// console.log(typeof obj);
+
+// console.log(obj.name);
+
+// console.log(2 + 2);
+
+// let mixin = "str" + 2;
+// console.log(typeof mixin);
+
+// let m = b = c = 3;
+
+// console.log(c);
+
+// =, ==, ===
+
+
+// let s = 2;
+// let b = "2";
+
+
+console.log(s === b);
+
+console.log(undefined == null);
+console.log(undefined === null);
+
+console.log(1 == true);
+console.log(0 == false);
+
+let s = 1,
+    b = 2,
+    c = 3,
+    g = 5;
+
+let a = 1;
+let j = 2;
+
