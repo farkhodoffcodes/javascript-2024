2435893a3aa2d990e33c3678cd4abe5c5572af4d
farkhodoff19 <farkhodoffcodes@gmail.com>
2023-12-05 04:50:33 -0800

    2-dars finished

---
+++ 2-dars/index.js
@@ -0,0 +1,137 @@
+
+
+
+// console.log(1 == "1");
+
+// let x = 1;
+// let y = 1;
+
+// console.log(x + y);
+
+// console.log(a);
+// console.log(x);
+
+// let x = 3;
+// var a = 4;
+
+
+// undefined == null
+
+
+// console.log(1 === "1");  
+// console.log(null == undefined);
+
+
+
+
+
+// let x = 1;
+// let a = 2;
+
+// console.log(x,a);
+
+
+
+
+// let elDesc = "lorem";
+// let kebab_case = "lorem";
+
+
+// let price = 12;
+
+// let priceDesc = "$";
+// console.log(price + priceDesc);
+
+
+// let price2 = 12;
+
+// let priceDesc2 = "$";
+// console.log(price2 + priceDesc2);
+
+
+// console.log("12$" + "12$");
+
+// let x = Number("1");
+
+// console.log(typeof x);
+
+// let result = "6" / "2"
+
+// console.log( typeof result );
+
+// let x = true;
+// console.log(typeof String(x));
+
+// let a = true + "";
+
+// console.log(typeof a);
+
+// truthy = "s", true, 1, " "
+// falsy = "", 0, false, null, undefined, !, 
+
+
+// console.log("3s" / 3);
+
+// console.log(isNaN("3"));
+
+// #1
+
+// let userInfo = prompt(Number("Yosh"));
+// let user = +prompt("Yosh", 12);
+
+
+// let a = 4 + 5;
+// console.log(a + 3);
+
+
+
+// console.log(5 / 2);
+
+// console.log(2 ** 3);
+
+// console.log(2 + 1);
+
+// let a = 2;
+// ++a;
+// console.log(a);
+// a++;
+// console.log(a);
+
+
+console.log(true || true); // true
+console.log(false || true); // true
+console.log(true || false); // true
+console.log(false || false); // false
+
+console.log(true && true); // true
+console.log(false &&  true); // false
+console.log(true && false); // false
+console.log(false && false); // false
+
+
+// console.log(!(1 || "" && false));
+
+// console.log( typeof !!null );
+
+// console.log("a" === "A");
+
+
+// console.log(!false);
+
+// console.log(undefined ?? null);
+
+// let x = prompt("Yoshingiz", 18)
+
+// if(x = 1) {
+// s
+// }else if(x > 1 && x < 20) {
+
+// }else {
+//     ""
+// }
+
+// 3 == "Fizz"
+// 5 == "Buzz"
+// 3,5 = "FizzBuzz"
+
+// 1 ? console.log(1) : console.log(0)
