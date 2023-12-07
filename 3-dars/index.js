6dc4fb62c45d4c5d71ac74d21c411a77d866f9e5
farkhodoff19 <farkhodoffcodes@gmail.com>
2023-12-07 05:14:18 -0800

    for loop, while, do while, switch case

---
+++ 3-dars/index.js
@@ -0,0 +1,80 @@
+
+
+// console.log(!(1 == true));
+
+let obj= {
+    hair: "curley",
+    eye:"red",
+    "hair": "stright"
+}
+
+
+// console.log(obj["hair"]); // stright
+// console.log(obj[hair]); // curley
+// console.log(obj[0]); // curley
+
+
+
+
+// let userInfo = 9;
+
+// if(userInfo % 3 === 0 && userInfo % 5 === 0) {
+//     console.log("FizzBuzz");
+// }else if (userInfo % 5 === 0){
+//     console.log("Buzz");
+// }else if (userInfo % 3 === 0) {
+//     console.log("Fizz");
+// }else {
+//     console.log("Something went wrong");
+// }
+
+// userInfo % 3 === 0 && userInfo % 5 === 0 ? console.log("FizzBzzz") ?  userInfo % 5 === 0
+ 
+// let user = 15;
+
+// switch(user) {
+//     case 15:
+//         console.log("FizzBuzz");
+//         break;
+//     case 3:
+//         console.log("Fizz");
+//         break;
+//     case 5:
+//         console.log("Buzz");
+//         break;
+//     default:
+//         console.log("User raqam kiritishi kerak");
+//         break;
+// }
+
+// let i = 1;
+// while(true == 1) {
+//     console.log(i);
+//     i++;
+// }
+
+
+// let i = 3;
+// do {
+//   console.log(i)
+//   i--;
+// } while (i >= 0);
+
+
+
+
+// for(let i = 1; i<= 10; i++) {
+//     console.log(i);
+//     for(let j = 1; i <= j; i++) {
+//         console.log(`i = ${i} va j = ${j}`);
+//     }
+
+// }
+
+
+// 1 karra jadval
+// 1 x 1 = 1
+
+
+// 2 karra jadval
+// 2 x 1 = 2
