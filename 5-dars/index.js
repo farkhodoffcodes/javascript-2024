f939eb0bfe5cb86af824338e3a725a73f2a7825e
farkhodoff19 <farkhodoffcodes@gmail.com>
2023-12-12 04:38:59 -0800

    ~~

---
+++ 5-dars/index.js
@@ -0,0 +1,241 @@
+// function calcMin(x,y) {
+//     if(x < y) {
+//         console.log(x)
+//     }else {
+//         console.log(y);
+//     }
+// }
+
+// function calcMax(x,y) {
+//     if(x > y) {
+//         console.log(x)
+//     }else {
+//         console.log(y);
+//     }
+// }
+
+
+// calcMax(1,2)
+// calcMin(-12,11);
+
+
+// function pow(a,b) {
+//     console.log(a ** b)
+// }
+
+// pow(2,5)
+
+
+
+// let salaries = {
+//     Ann: 200,
+//     John:200
+// }
+
+// let sum = 0;
+
+// for(let key in salaries) {
+//     sum += salaries[key]
+// }
+
+// console.log(sum);
+
+
+
+// let salaries = {
+//     Ann: 200,
+//     John:200,
+//     Smith:"200"
+// }
+
+// let sum = 0;
+
+// for(let key in salaries) {
+//     if(typeof salaries[key] === "number") {
+//         sum += salaries[key]
+//     }
+// }
+
+// console.log(sum);
+
+
+// "use strict"
+
+// console.log(a);
+// var a = 5;
+
+
+// console.log(a);
+// let a = 2;
+
+
+
+// function decloration() {
+//     return;
+// }
+
+
+
+// console.log(decloration() === undefined);
+
+
+// TypeConversion
+
+// STRING
+
+// let arr = [1,2,3]
+
+// console.log(typeof arr);
+
+// DATATYPE
+
+// STRING
+// UNDEFINED
+// NUMBER
+    // NAN
+    // INIFNITY
+// SYMBOL
+// BIGINT
+// BOOLEAN
+
+// NULL 
+// OBJECT 
+    // FUNCTION 
+    // ARRAY 
+
+
+
+    // console.log(1/0 && 1/0);
+    // console.log(Infinity || Infinity);
+    // console.log(1 == true);
+
+    // console.log();
+
+    // function dec() {
+
+    // }
+    // let sum = {
+
+    // }
+
+    // console.log(dec() === sum);
+    
+
+
+    // function s() {
+
+    //     return s;
+    // }
+
+    // console.log(typeof s());
+
+    // console.log(typeof function(){});
+
+
+    // console.log([1,2,3] === Object);
+
+    // let arr = [1,2,3];
+
+    // console.log(typeof arr);
+
+    // [[ ] => []]
+
+    // new Array
+    // Array
+
+    // () => {}
+
+    // let user = "John";
+
+    // let a = 5;
+    // let b = 10;
+
+    // console.log(a + b);
+
+    // let result = "Hello" +  user
+    // let result2 = `Hello ${user}`
+
+    // ??
+
+    // ||
+    // &&
+
+    // let ratio = 6.6
+    // let a = 6
+
+    // console.log(~~ratio == a);
+
+//    true && false
+//    false && true
+
+
+//    console.log("" && 1);
+//    console.log("" && "1");
+//    console.log(!("" && "1"));
+//    console.log(!("" || "1"));
+
+//    console.log(undefined ?? null);
+//    console.log("1" ?? null);
+
+//    let a = Symbol("id")
+//    let b = Symbol("id")
+
+//    let s = "2"
+//    let g = "2"
+
+//    console.log(a === b);
+//    console.log(s=== g);
+
+
+// console.log(arr);
+// let arr = ["Apple", "Kiwi"];
+
+
+// console.log(arr);
+
+// arr[1] = "Banana"
+
+// console.log(arr);
+
+
+// s("Hello");
+// let s = function(a) {
+//     return a
+// }
+
+// dec("Hello")
+// function dec(s) {
+//     console.log(s);
+// }
+
+
+// let arr = [1,2,3,56,7,8,8,9,9,];
+
+// console.log(arr.length());
+// console.log(arr.length);
+
+
+
+   
+//    console.log(a);
+//    var a = 2
+
+//    function s() {
+//     return;
+//    }
+
+//    s()
+
+
+// let obj = {
+//     John:2,
+//     Ann:3,
+// }
+
+// for(let key in obj) {
+//     console.log(`Key ${key} value ${obj[key]}`);
+// }
+
+// let arr = [1,2,3 [4,5,6]]
+// let arr2 = [1,2,3,4,5]
+
