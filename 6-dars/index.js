590ae35ac2eab847e9b7588a2ceb4ad41097b60c
farkhodoff19 <farkhodoffcodes@gmail.com>
2023-12-14 04:26:52 -0800

    Function expression & arrow function

---
+++ 6-dars/index.js
@@ -0,0 +1,52 @@
+// let result = +"" + 1
+// console.log(result);
+
+// TYPE CONVERSION
+// let sym = Symbol("id")
+// let sym2 = Symbol("id")
+
+
+// console.log(sym === sym2);
+
+
+// let arr = [1,2,3];
+
+// console.log(arr); 
+
+// arr[0] = 4;
+
+// console.log(arr); 
+
+// let a = " ";
+// console.log(a === "string");
+// let result = "2" * "3"
+
+// console.log( typeof result);
+
+
+
+
+// function calcPow(a,b) {
+//     if(a > b) {
+
+//     }else {
+
+//     }
+// }
+
+// "use strict"
+
+// console.log(a);
+// a = 2;
+// console.log(a);
+
+// console.log(a);
+// let a = 2;
+
+// const getInfo = function() {
+
+//    let user =  prompt("Yoshingiz");
+//    alert(typeof user)
+// }
+
+
