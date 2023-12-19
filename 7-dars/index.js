b9c171894bc81292624d1ad398ca6852af597624
farkhodoff19 <farkhodoffcodes@gmail.com>
2023-12-19 04:40:20 -0800

    8-dars

---
+++ 7-dars/index.js
@@ -0,0 +1,99 @@
+// let obj = {
+//     0: 11,
+// }
+
+// console.log(obj[0]);
+// console.log(obj["0"]);
+
+
+
+// console.log(1 ?? undefined ?? 2 );
+// console.log(1 ?? undefined );
+
+
+// let elHeading = document.getElementById("heading");
+// console.log(elHeading);
+
+// let a = document.getElementsByTagNameNS("", "")
+
+
+// let person = ["John", 20, 'dasturchi', "oliy malumotli", "muqaddam sudlanmagan", "opasniy chelovek"];
+
+// console.log(person);
+
+// let person = {
+//     name: "John",
+//     age: 20,
+//     subject: "Developer"
+// }
+
+
+
+// alert(person);
+
+// let a = {}
+// let b = new Object();
+
+// console.log(a == b);
+// console.log(a === b);
+
+// let a = "2";
+// let b = "2";
+
+
+// console.log(a == b);
+// console.log(a === b);
+
+
+// let obj = {
+//     name: "John",
+//     "birth-year": 1990,
+//     4: "Hello",
+//     22: "Jahonsher",
+//     "22": "A"
+// }
+
+// let key = "name";
+
+// obj.first = "Hello"
+
+// console.log(obj["name"]);
+// console.log(obj.name);
+// console.log(obj[key]);
+
+// console.log(obj[2 + 2]);
+// console.log(obj["2" + "2"])
+// console.log(obj["22"]);
+// console.log(obj[`${2 + 2}`]);
+
+// console.log(obj[key]); 
+// console.log(obj["key"]);
+
+// console.log(obj.birth-year);
+// console.log(obj."birth-year");
+// console.log(obj["birth-year"]); // 1990
+// console.log(obj[birth-year]); 
+
+
+
+// delete
+// console.log(obj);
+
+// delete obj["name"];
+
+// console.log(obj);
+
+
+// let a = 2;
+// let age = 17;
+
+// let obj = {
+//     a,
+//     age,
+// }
+
+
+// obj.a = "Hello"
+// obj.age = 10
+
+// console.log(obj);
